20fe2ccc0a30b418afaa09c6988fa2cb19d3c7fd
Michael Cooper <mc0197@uah.edu>
2024-04-18 23:07:57 -0500

    test push alerts

--- db.py
+++ db.py
@@ -331,3 +331,49 @@ def remove_role(id): #remove role by id
 
     db.execute('DELETE FROM Roles WHERE role_id = ?', (id,)) #then delete the role
     db.commit()
+    
+    #alerts.py functions 
+    
+def add_alert(db, user_id, message): # Add a new alert for a user
+    db.execute('INSERT INTO alerts (user_id, message) VALUES (?, ?)',
+               (user_id, message))
+    db.commit()
+
+def get_alerts(db, user_id): # Retrieve all alerts for a user
+    return db.execute('SELECT * FROM alerts WHERE user_id = ?', (user_id,)).fetchall()
+
+
+def update_document_state(document_id, new_state):
+    # Update doc state
+    datagate = DataGate()
+    current_state = datagate.get_document_state(document_id)
+
+    if current_state != new_state:
+        datagate.update_document_state(document_id, new_state)
+        generate_alert(document_id, new_state)
+
+def generate_alert(document_id, new_state):
+    # Generate/store alert
+    datagate = DataGate()
+    alert_message = f"Document {document_id} state changed to {STATES[new_state].name}"
+    recipient_id = datagate.get_document_owner(document_id)
+    datagate.addAlert(recipient_id, alert_message)
+
+# DataGate extensions
+def DataGate_get_document_state(self, document_id):
+    # Fetch doc state
+    query = "SELECT document_stage FROM Document WHERE document_id=?"
+    result = self.execute_query(query, (document_id,))
+    return result.fetchone()[0]
+
+def DataGate_update_document_state(self, document_id, new_state):
+    # Update doc state in DB
+    query = "UPDATE Document SET document_stage=? WHERE document_id=?"
+    self.execute_query(query, (new_state, document_id))
+    self.connection.commit()
+
+def DataGate_get_document_owner(self, document_id):
+    # Fetch doc owner
+    query = "SELECT assigned_employee FROM Document WHERE document_id=?"
+    result = self.execute_query(query, (document_id,))
+    return result.fetchone()[0]
